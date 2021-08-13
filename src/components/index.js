fc0c174f062afe31e0203e968cb3e59e559aa682
aubyn <aubyn@vip.qq.com>
2021-08-13 09:22:28 +0800

    code format

--- src/components/index.js
+++ src/components/index.js
@@ -6,7 +6,7 @@ const c = compReq.keys().reduce((t, c) => {
 }, {})
 export default {
   install(app) {
-    Object.keys(c).forEach((key) => {
+    Object.keys(c).forEach(key => {
       app.component(key, c[key])
     })
   },
