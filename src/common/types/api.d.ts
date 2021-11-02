cfb65690fb0a9bb8860d59a86219e47621d05841
aubyn <aubyn@vip.qq.com>
2021-11-02 11:09:53 +0800

    apply router.prototype

--- src/common/types/api.d.ts
+++ src/common/types/api.d.ts
@@ -1,4 +1,9 @@
 import Vue from 'vue'
+declare module 'vue-router/types/router' {
+  interface VueRouter {
+    linkTo(to: RawLocation, opts: { current?: Route; append?: boolean; type?: '_blank' | '_parent' | '_self' | '_top' }): void
+  }
+}
 declare module 'vue/types/vue' {
   interface Vue {
     $api: () => {}
--- src/router/index.js
+++ src/router/index.js
@@ -11,4 +11,15 @@ const router = new VueRouter({
   routes,
 })
 
+VueRouter.prototype.linkTo = function (to, { current, append, type } = {}) {
+  if (typeof arguments[0] != 'object') return
+  type = type || '_blank'
+  console.log(this)
+  const routeData = this.resolve.apply(
+    this,
+    [to, current, append].filter(i => typeof i !== 'undefined')
+  )
+  window.open(routeData.href, type)
+}
+
 export default router
