c679a5ec09e744bbab0534ea04c077d0152564ed
aubyn <aubyn@vip.qq.com>
2021-11-08 16:04:47 +0800

    change element

--- src/common/plugins/element.js
+++ src/common/plugins/element.js
@@ -5,4 +5,14 @@ locale.use(lang)
 export default function (app) {
   app.prototype.$ELEMENT = { size: 'small' }
   app.use(Button)
+  app.prototype.$validateFields = function (form, props) {
+    return new Promise((resolve, reject) => {
+      let isFail = false
+      form.validateField(props, res => {
+        if (res) isFail = true
+      })
+      if (isFail) return reject(false)
+      resolve(true)
+    })
+  }
 }
